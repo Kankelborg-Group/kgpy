85930d69e6eb4abd66f1b97fad8d86403139b137
Roy Smart <roytsmart@gmail.com>
2019-08-24 16:04:48 -0600

    Further work on packaging.

--- setup.py
+++ setup.py
@@ -5,14 +5,17 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'kgpy @ git+https://titan.ssel.montana.edu/gitlab/Kankelborg-Group/kgpy.git#egg=kgpy'
-        'numpy'
-        'matplotlib'
-        'scipy'
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'astropy'
+        'beautifultable'
+        'numpy-quaternion'
+        'pywin32 ; platform_system=="Windows"'
     ],
     url='https://titan.ssel.montana.edu/gitlab/Kankelborg-Group/kgpy',
     license='',
     author='Roy Smart',
     author_email='roytsmart@gmail.com',
-    description='Repository for the Full-disk Ultraviolet Rocket Spectrometer'
+    description='Repository for the Kankelborg Group\'s common codebase'
 )
