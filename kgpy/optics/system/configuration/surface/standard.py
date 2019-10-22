d92474e5b1f1b04ac7dbce07c99a76aa35a4e47b
Roy Smart <roytsmart@gmail.com>
2019-10-22 09:36:22 -0600

    - Starting work on distortion function.

--- kgpy/optics/system/configuration/surface/standard.py
+++ kgpy/optics/system/configuration/surface/standard.py
@@ -1,4 +1,4 @@
-
+import dataclasses
 import numpy as np
 import astropy.units as u
 
@@ -9,71 +9,12 @@ from . import Surface, Material, Aperture
 __all__ = ['Standard']
 
 
+@dataclasses.dataclass
 class Standard(Surface):
 
-    def __init__(self,
-                 *args,
-                 radius: u.Quantity = None,
-                 material: Material = None,
-                 conic: u.Quantity = None,
-                 aperture: Aperture = None,
-                 pre_tilt_decenter: math.geometry.CoordinateSystem = None,
-                 post_tilt_decenter: math.geometry.CoordinateSystem = None,
-                 **kwargs
-                 ):
-
-        super().__init__(*args, **kwargs)
-
-        if radius is None:
-            radius = np.inf * u.m
-        if material is None:
-            material = optics.system.configuration.surface.material.EmptySpace()
-        if conic is None:
-            conic = 0 * u.dimensionless_unscaled
-        if pre_tilt_decenter is None:
-            pre_tilt_decenter = math.geometry.CoordinateSystem()
-        if post_tilt_decenter is None:
-            post_tilt_decenter = math.geometry.CoordinateSystem()
-
-        self._radius = radius
-        self._material = material
-        self._conic = conic
-        self._aperture = aperture
-        self._pre_tilt_decenter = pre_tilt_decenter
-        self._post_tilt_decenter = post_tilt_decenter
-
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def thickness(self):
-        return self._thickness
-
-    @property
-    def radius(self):
-        return self._radius
-
-    @property
-    def material(self) -> Material:
-        return self._material
-
-    @property
-    def conic(self) -> u.Quantity:
-        return self._conic
-
-    @property
-    def aperture(self) -> Aperture:
-        return self._aperture
-
-    @property
-    def pre_tilt_decenter(self) -> math.geometry.CoordinateSystem:
-        return self._pre_tilt_decenter
-
-    @property
-    def post_tilt_decenter(self) -> math.geometry.CoordinateSystem:
-        return self._post_tilt_decenter
-
-
-
+    radius: u.Quantity = 0 * u.mm
+    conic: u.Quantity = 0 * u.dimensionless_unscaled
+    material: Material = None
+    aperture: Aperture = None
+    pre_tilt_decenter: math.geometry.CoordinateSystem = None,
+    post_tilt_decenter: math.geometry.CoordinateSystem = None,
--- kgpy/optics/system/configuration/surface/surface.py
+++ kgpy/optics/system/configuration/surface/surface.py
@@ -1,4 +1,4 @@
-
+import dataclasses
 import typing as tp
 import numpy as np
 import astropy.units as u
@@ -11,114 +11,13 @@ from . import Aperture, Material
 __all__ = ['Surface']
 
 
+@dataclasses.dataclass
 class Surface:
     """
     This class represents a single optical surface. This class should be a drop-in replacement for a Zemax surface, and
     have all the same properties and behaviors.
     """
 
-    def __init__(self,
-                 name: str = '',
-                 is_stop: bool = False,
-                 thickness: u.Quantity = None,
-                 ):
-
-        if thickness is None:
-            thickness = 0 * u.m
-
-        self._name = name
-        self._is_stop = is_stop
-        self._thickness = thickness
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @property
-    def is_stop(self) -> bool:
-        return self._is_stop
-
-    @property
-    def thickness(self) -> u.Quantity:
-        return self._thickness
-
-
-    @property
-    def table_headers(self) -> tp.List[str]:
-        """
-        List of headers used for printing table representation of surface
-
-        :return: List of header strings
-        """
-        return ['Component', 'Surface', 'Thickness', '',
-                'X_x', 'X_y', 'X_z', '',
-                'T_x', 'T_y', 'T_z', '',
-                'Is Stop']
-
-    @property
-    def table_row(self):
-        """
-        Format the surface as a list of strings for use in the table representation.
-
-        :return: List of strings representing the surface.
-        """
-
-        # Only populate the component name string if the component exists
-        if self.component is not None:
-            comp_name = self.component.name
-        else:
-            comp_name = ''
-
-        # Store shorthand versions of coordinate system variables
-        cs = self.cs
-        X = cs.translation
-        Q = cs.rotation
-
-        # Convert to Tait-Bryant angles
-        T = math.geometry.quaternion.quaternion.as_zyx_intrinsic_tait_bryan_angles(Q) * u.rad    # type: u.Quantity
-
-        # Convert angles to degrees
-        T = T.to(u.deg)
-
-        def fmt(s) -> str:
-            """
-            Format a number for use with the table representation
-
-            :param s: Number to be formatted
-            :return: String representation of the number in the specified format.
-            """
-            return "{0:.2f}".format(s)
-
-        return [comp_name, self.name, fmt(self.thickness), '',
-                fmt(X.x), fmt(X.y), fmt(X.z), '',
-                fmt(T[0]), fmt(T[1]), fmt(T[2]), '',
-                self.is_stop]
-
-    def __eq__(self, other: 'Surface'):
-        """
-        Check if two surface are equal by comparing all of their attributes
-        :param other: The other surface to check against this one.
-        :return: True if the two surfaces have the same values for all attributes, false otherwise.
-        """
-        a = self.name == other.name
-        # b = self.comment == other.comment
-        # d = self.thickness == other.thickness
-        e = self.component == other.component
-
-        return a and e
-
-    def __str__(self) -> str:
-        """
-        :return: String representation of the surface
-        """
-
-        # Construct new table for printing
-        table = BeautifulTable(max_width=200)
-
-        # Populate table
-        table.column_headers = self.table_headers
-        table.append_row(self.table_row)
-
-        return table.get_string()
-
-    __repr__ = __str__
+    name: str = ''
+    is_stop: bool = False
+    thickness: u.Quantity = None
