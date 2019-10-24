d96274c999d184e142ed7c90805217f7eec7d7bd
byrdie <roytsmart@gmail.com>
2019-10-24 15:42:30 -0600

    - Updated multi-surface.

--- kgpy/optics/system/surface/multi_surface.py
+++ kgpy/optics/system/surface/multi_surface.py
@@ -1,61 +1,118 @@
-
+import dataclasses
 import typing as tp
+import nptyping as npt
 import astropy.units as u
 
-from kgpy import math
-from . import Aperture, Surface, CoordinateBreak
+from . import Material, Aperture, Standard, CoordinateBreak
 
 __all__ = ['MultiSurface']
 
 
+@dataclasses.dataclass
 class MultiSurface:
 
+    coordinate_break_before: CoordinateBreak = CoordinateBreak()
+    aperture_surface: Standard = Standard()
+    main_surface: Standard = Standard()
+    coordinate_break_after_z: CoordinateBreak = CoordinateBreak()
+    coordinate_break_after: CoordinateBreak = CoordinateBreak()
 
+    @classmethod
+    def from_surface_params(
+            cls,
+            name: tp.Union[str, npt.Array[str]] = '',
+            is_stop: tp.Union[bool, npt.Array[bool]] = False,
+            thickness: u.Quantity = 0 * u.mm,
+            radius: u.Quantity = 0 * u.mm,
+            conic: u.Quantity = 0 * u.dimensionless_unscaled,
+            material: tp.Union[tp.Optional[Material], npt.Array[tp.Optional[Material]]] = None,
+            aperture: tp.Union[tp.Optional[Aperture], npt.Array[tp.Optional[Aperture]]] = None,
+            mechanical_aperture: tp.Union[tp.Optional[Aperture], npt.Array[tp.Optional[Aperture]]] = None,
+            decenter_before: u.Quantity = [0, 0, 0] * u.m,
+            decenter_after: u.Quantity = [0, 0, 0] * u.m,
+            tilt_before: u.Quantity = [0, 0, 0] * u.deg,
+            tilt_after: u.Quantity = [0, 0, 0] * u.deg,
+    ):
 
-    def __init__(self, name: str):
+        cb_before = CoordinateBreak(
+            name=name + '.cb_before',
+            tilt=tilt_before,
+            decenter=decenter_before,
+        )
 
-        self._pre_tilt_decenter = CoordinateBreak(name + '.pre_coordinate_break')
-        self._aperture = Surface(name + '.aperture')
-        self._mechanical_aperture = Surface(name + '.mechanical_aperture')
-        self._post_tilt_decenter = CoordinateBreak(name + '.post_coordinate_break')
-        self._thickness = Surface(name + '.post_coordinate_break')
+        aper_surf = Standard(
+            name=name + '.aperture',
+            aperture=aperture,
+        )
+
+        main_surf = Standard(
+            name=name,
+            is_stop=is_stop,
+            radius=radius,
+            conic=conic,
+            material=material,
+            aperture=mechanical_aperture,
+        )
+
+        # Zemax doesn't allow decenters in z, instead you're supposed to use the `thickness` parameter.
+        # Unfortunately, the `thickness` parameter is always applied last, regardless of the `order` parameter of the
+        # surface.
+        # As a workaround, we can add an extra surface to take care of the translation in z, before the rest of the
+        # tilts/decenters are applied.
+        decenter_after_z = decenter_after.copy()
+        decenter_after_z[..., :~0] = 0
+        decenter_after[..., ~0] = 0
+
+        cb_after_z = CoordinateBreak(
+            name=name + '.cb_after_z',
+            decenter=decenter_after_z,
+        )
+
+        cb_after = CoordinateBreak(
+            name=name+'.cb_after',
+            thickness=thickness,
+            tilt=tilt_after,
+            decenter=decenter_after,
+        )
+
+        return cls(cb_before, aper_surf, main_surf, cb_after_z, cb_after)
 
     @property
-    def aperture(self) -> Aperture:
-        return self._aperture.aperture
-
-    @aperture.setter
-    def aperture(self, value: Aperture):
-        self.aperture.aperture = value
+    def name(self):
+        return self.main_surface.name
 
     @property
-    def mechanical_aperture(self) -> Aperture:
-        return self._mechanical_aperture.aperture
-
-    @mechanical_aperture.setter
-    def mechanical_aperture(self, value: Aperture):
-        self._mechanical_aperture.aperture = value
-        
-    @property
-    def pre_tilt_decenter(self) -> math.geometry.CoordinateSystem:
-        return self._pre_tilt_decenter.pre_tilt_decenter
-    
-    @pre_tilt_decenter.setter
-    def pre_tilt_decenter(self, value: math.CoordinateSystem):
-        self._pre_tilt_decenter.pre_tilt_decenter = value
+    def is_stop(self):
+        return self.main_surface.is_stop
 
     @property
-    def post_tilt_decenter(self) -> math.CoordinateSystem:
-        return self._post_tilt_decenter.post_tilt_decenter
-
-    @post_tilt_decenter.setter
-    def post_tilt_decenter(self, value: math.CoordinateSystem):
-        self._post_tilt_decenter.post_tilt_decenter = value
+    def thickness(self):
+        return self.coordinate_break_after.thickness
 
     @property
-    def thickness(self) -> u.Quantity:
-        return self._thickness.thickness
+    def radius(self):
+        return self.main_surface.radius
 
-    @thickness.setter
-    def thickness(self, value: u.Quantity):
-        self._thickness.thickness = value
+    @property
+    def conic(self):
+        return self.main_surface.conic
+
+    @property
+    def material(self):
+        return self.main_surface.material
+
+    @property
+    def decenter_before(self):
+        return self.coordinate_break_before.decenter
+
+    @property
+    def decenter_after(self):
+        return self.coordinate_break_after.decenter + self.coordinate_break_after_z.decenter
+
+    @property
+    def tilt_before(self):
+        return self.coordinate_break_before.tilt
+
+    @property
+    def tilt_after(self):
+        return self.coordinate_break_after.tilt
