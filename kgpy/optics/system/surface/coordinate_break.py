58123f2557547c184d2966edd41eb22fefaff427
byrdie <roytsmart@gmail.com>
2019-10-24 15:43:40 -0600

    - Added `translation_first` parameter to store order of operations.

--- kgpy/optics/system/surface/coordinate_break.py
+++ kgpy/optics/system/surface/coordinate_break.py
@@ -8,7 +8,10 @@ from . import Surface
 __all__ = ['CoordinateBreak']
 
 
+@dataclasses.dataclass
 class CoordinateBreak(Surface):
 
     decenter: u.Quantity = [0, 0, 0] * u.m
     tilt: u.Quantity = [0, 0, 0] * u.deg
+
+    translation_first: bool = True
