b588e2263e9d2e49b15d6c3cbfa6b8eef90f8cfa
Roy Smart <roytsmart@gmail.com>
2019-10-24 20:37:42 -0600

    - Created elliptical grating surface for use with esis.

---
+++ kgpy/optics/system/surface/elliptical_grating.py
@@ -0,0 +1,17 @@
+import dataclasses
+import typing as tp
+import astropy.units as u
+
+from . import DiffractionGrating
+
+__all__ = ['EllipticalGrating1']
+
+
+@dataclasses.dataclass
+class EllipticalGrating1(DiffractionGrating):
+
+    a: u.Quantity = 0 * u.dimensionless_unscaled
+    b: u.Quantity = 0 * u.dimensionless_unscaled
+    c: u.Quantity = 0 * u.m
+    alpha: u.Quantity = 0 * u.dimensionless_unscaled
+    beta: u.Quantity = 0 * u.dimensionless_unscaled
