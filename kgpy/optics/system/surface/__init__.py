aa9870f8d9da9de1a380d3ae58416056c12a6b34
Roy Smart <roytsmart@gmail.com>
2019-10-24 20:40:48 -0600

    - Expose classes to prevent stutter.

--- kgpy/optics/system/surface/__init__.py
+++ kgpy/optics/system/surface/__init__.py
@@ -5,5 +5,6 @@ from .standard import Standard
 from .coordinate_break import CoordinateBreak
 from .diffraction_grating import DiffractionGrating
 from .toroidal import Toroidal
+from .elliptical_grating import EllipticalGrating1
 
 from . import aperture
