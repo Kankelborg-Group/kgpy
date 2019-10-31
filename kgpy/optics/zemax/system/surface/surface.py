fd3dc1d082ba822615a787e4b5507ea5ade69f97
Roy Smart <roytsmart@gmail.com>
2019-10-31 11:00:53 -0600

    - Vectorizing the surface module.

--- kgpy/optics/zemax/system/surface/surface.py
+++ kgpy/optics/zemax/system/surface/surface.py
@@ -1,18 +1,43 @@
-
 import typing as tp
 import astropy.units as u
 
+from kgpy.optics import system
 from kgpy.optics.zemax import ZOSAPI
+from .. import util
 from kgpy.optics.zemax.system.surface.coordinate_break import add_coordinate_break_surface_to_zemax_system
 from kgpy.optics.zemax.system.surface.diffraction_grating import add_diffraction_grating_to_zemax_system
 from kgpy.optics.zemax.system.surface.standard import add_standard_surface_to_zemax_system
 from kgpy.optics.zemax.system.surface.toroidal import add_toroidal_surface_to_zemax_system
 
 
-def add_surfaces_to_zemax_system(zemax_system: ZOSAPI.IOpticalSystem,
-                                 zemax_units: u.Unit,
-                                 configuration_index: int,
-                                 surfaces: 'tp.List[optics.system.configuration.Surface]'):
+def add_surfaces_to_zemax_system(
+        zemax_system: ZOSAPI.IOpticalSystem,
+        surfaces: 'tp.List[system.Surface]',
+        configuration_shape: tp.Tuple[int],
+        zemax_units: u.Unit,
+
+):
+
+    op_comment = ZOSAPI.Editors.MCE.MultiConfigOperandType.MCOM
+    op_thickness = ZOSAPI.Editors.MCE.MultiConfigOperandType.THIC
+
+    unit_thickness = zemax_units
+
+    num_surfaces = len(surfaces)
+    while zemax_system.LDE.NumberOfSurfaces < num_surfaces:
+        zemax_system.LDE.AddSurface()
+    
+    for s in range(num_surfaces):
+        
+        surface_index = s + 1
+        surface = surfaces[s]
+        
+        util.set_str(zemax_system, surface.name, configuration_shape, op_comment)
+        util.set_float(zemax_system, surface.thickness, configuration_shape, op_thickness, unit_thickness, 
+                       surface_index)
+        
+        # if surface
+        
     surface_op_types = [
         ZOSAPI.Editors.MCE.MultiConfigOperandType.MCOM,
         ZOSAPI.Editors.MCE.MultiConfigOperandType.THIC,
@@ -24,14 +49,13 @@ def add_surfaces_to_zemax_system(zemax_system: ZOSAPI.IOpticalSystem,
 
         if configuration_index == 0:
 
-            if surface_index >= zemax_system.LDE.NumberOfSurfaces - 1 <= surface_index < len(surfaces) - 1:
+            if surface_index >= zemax_system.LDE.NumberOfSurfaces - 1 <= surface_index < num_surfaces - 1:
                 zemax_system.LDE.AddSurface()
 
             for op_type in surface_op_types:
                 op = zemax_system.MCE.AddOperand()
                 op.ChangeType(op_type)
                 op.Param1 = surface_index
-
 
         zemax_surface = zemax_system.LDE.GetSurfaceAt(surface_index)
 
@@ -48,22 +72,13 @@ def add_surfaces_to_zemax_system(zemax_system: ZOSAPI.IOpticalSystem,
                                                  surface_index, surface)
 
             if isinstance(surface, kgpy.optics.system.surface.DiffractionGrating):
-
                 add_diffraction_grating_to_zemax_system(zemax_system, zemax_surface, configuration_index, surface_index,
                                                         surface)
 
             if isinstance(surface, kgpy.optics.system.surface.Toroidal):
-
                 add_toroidal_surface_to_zemax_system(zemax_system, zemax_surface, zemax_units, configuration_index,
                                                      surface_index, surface)
 
         if isinstance(surface, kgpy.optics.system.surface.CoordinateBreak):
-
             add_coordinate_break_surface_to_zemax_system(zemax_system, zemax_surface, zemax_units, configuration_index,
                                                          surface_index, surface)
-
-
-
-
-
-
--- kgpy/optics/zemax/system/util.py
+++ kgpy/optics/zemax/system/util.py
@@ -38,3 +38,32 @@ def set_float(
         config_index = i + 1
         cell = op.GetOperandCell(config_index)
         cell.DoubleValue = value
+
+
+def set_str(
+        zemax_system: ZOSAPI.IOpticalSystem,
+        value: tp.Union[str, npt.Array[str]],
+        configuration_shape: tp.Tuple[int],
+        op_type: ZOSAPI.Editors.MCE.MultiConfigOperandType,
+        param_1: tp.Optional[int] = None,
+        param_2: tp.Optional[int] = None,
+        param_3: tp.Optional[int] = None,
+):
+    value_broadcasted = np.broadcast_to(value, configuration_shape).flat
+
+    op = zemax_system.MCE.AddOperand()
+    op.ChangeType(op_type)
+
+    if param_1 is not None:
+        op.Param1 = param_1
+
+    if param_2 is not None:
+        op.Param2 = param_2
+
+    if param_3 is not None:
+        op.Param3 = param_3
+    
+    for i, value in value_broadcasted:
+        config_index = i + 1
+        cell = op.GetOperandCell(config_index)
+        cell.Value = value
