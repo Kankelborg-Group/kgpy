8d74dd8717d4ac3925f9f7e42834a9d03b3eee7d
Roy Smart <roytsmart@gmail.com>
2019-11-05 14:34:06 -0700

    Starting work on distortion model.

--- kgpy/optics/zemax/ZOSAPI/Tools/ISystemTool.py
+++ kgpy/optics/zemax/ZOSAPI/Tools/ISystemTool.py
@@ -1,2 +1,3 @@
 class ISystemTool:
-    pass
+    def RunAndWaitForCompletion(self):
+        pass
--- kgpy/optics/zemax/system/rays.py
+++ kgpy/optics/zemax/system/rays.py
@@ -14,8 +14,7 @@ def trace(
         configuration_indices: tp.Optional[tp.List[int]] = None,
         surface_indices: tp.List[int] = (~0,),
         wavelength_indices=None,
-
-):
+) -> tp.Tuple:
     if isinstance(num_field, int):
         num_field = num_field, num_field
 
@@ -36,25 +35,58 @@ def trace(
     if wavelength_indices is None:
         wavelength_indices = np.arange(zemax_system.SystemData.Wavelengths.NumberOfWavelengths)
 
-    old_config = zemax_system.MCE.CurrentConfiguration
+    starting_config = zemax_system.MCE.CurrentConfiguration
 
     total_num_rays = 100 * 100
 
-    for c in configuration_indices:
+    sh = (
+        len(configuration_indices),
+        len(surface_indices),
+        len(wavelength_indices),
+    )
 
-        zemax_system.MCE.SetCurrentConfiguration(c + 1)
+    sh += fg_x.shape
+
+    out_x = np.empty(sh)
+    out_y = np.empty(sh)
+    out_mask = np.empty(sh)
+
+    for c, config_index in enumerate(configuration_indices):
+
+        zemax_system.MCE.SetCurrentConfiguration(config_index + 1)
 
         rt = zemax_system.Tools.OpenBatchRayTrace()
         tool = zemax_system.Tools.CurrentTool
 
-        for s in surface_indices:
+        for s, surf_index in enumerate(surface_indices):
 
-            rt_dat = rt.CreateNormUnpol(total_num_rays, ZOSAPI.Tools.RayTrace.RaysType.Real, s + 1)
+            rt_dat = rt.CreateNormUnpol(total_num_rays, ZOSAPI.Tools.RayTrace.RaysType.Real, surf_index + 1)
 
-            for w in wavelength_indices:
+            for w, wavl_index in enumerate(wavelength_indices):
 
-                pass
+                for ix, fx in enumerate(fg_x):
+                    for iy, fy in enumerate(fg_y):
 
+                        for jx, px in enumerate(pg_x):
+                            for jy, py in enumerate(pg_y):
+                                if mask[c, s, w, ix, iy, jx, jy] == 1:
+                                    rt_dat.AddRay(wavl_index + 1, fx, fy, px, py, ZOSAPI.Tools.RayTrace.OPDMode.None_)
 
+                            tool.RunAndWaitForCompletion()
+                            rt_dat.StartReadingResults()
 
-    zemax_system.MCE.SetCurrentConfiguration(old_config)
+                            for jy, py in enumerate(pg_y):
+                                if mask[c, s, w, ix, iy, jx, jy] == 1:
+                                    zemax_ray = rt_dat.ReadNextResult()
+                                    ret, n, err, vig, x, y, z, l, m, n, l2, m2, n2, opd, intensity = zemax_ray
+                                    out_mask[c, s, w, ix, iy, jx, jy] -= err + vig
+                                    out_x[c, s, w, ix, iy, jx, jy] = x
+                                    out_y[c, s, w, ix, iy, jx, jy] = y
+
+                            rt_dat.ClearData()
+
+        tool.Close()
+
+    zemax_system.MCE.SetCurrentConfiguration(starting_config)
+
+    return out_x, out_y, out_mask
