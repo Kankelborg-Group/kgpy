47f2f5ed98dbc233b78bdc93ca1b8d834814271b
Roy Smart <roytsmart@gmail.com>
2019-11-06 09:02:37 -0700

    Working on outputting correct rays.

--- kgpy/optics/zemax/system/rays.py
+++ kgpy/optics/zemax/system/rays.py
@@ -2,6 +2,7 @@ import typing as tp
 import numpy as np
 import nptyping as npt
 import astropy.units as u
+import numba
 
 from kgpy.optics.zemax import ZOSAPI
 
@@ -38,7 +39,9 @@ def trace(
     if configuration_indices is None:
         configuration_indices = np.arange(zemax_system.MCE.NumberOfConfigurations)
 
-    surface_indices = np.array(surface_indices)
+    surface_indices = np.array(surface_indices) % zemax_system.LDE.NumberOfSurfaces
+    print(surface_indices)
+
     if wavelength_indices is None:
         wavelength_indices = np.arange(zemax_system.SystemData.Wavelengths.NumberOfWavelengths)
 
@@ -56,7 +59,7 @@ def trace(
 
     out_x = np.empty(sh)
     out_y = np.empty(sh)
-    out_mask = np.empty(sh)
+    out_mask = np.empty(sh, dtype=int)
 
     for c, config_index in enumerate(configuration_indices):
 
@@ -67,26 +70,27 @@ def trace(
 
         for s, surf_index in enumerate(surface_indices):
 
-            rt_dat = rt.CreateNormUnpol(total_num_rays, ZOSAPI.Tools.RayTrace.RaysType.Real, surf_index + 1)
+            rt_dat = rt.CreateNormUnpol(total_num_rays, ZOSAPI.Tools.RayTrace.RaysType.Real, surf_index + 2)
 
             for w, wavl_index in enumerate(wavelength_indices):
 
-                for ix, fx in enumerate(fg_x):
-                    for iy, fy in enumerate(fg_y):
+                for ix, fx in enumerate(field_x):
+                    for iy, fy in enumerate(field_y):
 
-                        for jx, px in enumerate(pg_x):
-                            for jy, py in enumerate(pg_y):
-                                if mask[c, s, w, ix, iy, jx, jy] == 1:
+                        for jx, px in enumerate(pupil_x):
+                            for jy, py in enumerate(pupil_y):
+                                if mask[ix, iy, jx, jy] == 1:
+
                                     rt_dat.AddRay(wavl_index + 1, fx, fy, px, py, ZOSAPI.Tools.RayTrace.OPDMode.None_)
 
                             tool.RunAndWaitForCompletion()
                             rt_dat.StartReadingResults()
 
                             for jy, py in enumerate(pg_y):
-                                if mask[c, s, w, ix, iy, jx, jy] == 1:
+                                if mask[ix, iy, jx, jy] == 1:
                                     zemax_ray = rt_dat.ReadNextResult()
                                     ret, n, err, vig, x, y, z, l, m, n, l2, m2, n2, opd, intensity = zemax_ray
-                                    out_mask[c, s, w, ix, iy, jx, jy] -= err + vig
+                                    out_mask[c, s, w, ix, iy, jx, jy] = int(not(err == 0 and vig == 0))
                                     out_x[c, s, w, ix, iy, jx, jy] = x
                                     out_y[c, s, w, ix, iy, jx, jy] = y
 
