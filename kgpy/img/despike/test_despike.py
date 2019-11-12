14909a3c77ce1a31a469a0f42b425abbd099e5e0
byrdie <roytsmart@gmail.com>
2019-11-12 10:54:24 -0600

    - Removing old code.

--- kgpy/img/despike/test_despike.py
+++ kgpy/img/despike/test_despike.py
@@ -10,21 +10,6 @@ from . import despike
 testfile = pathlib.Path(kgpy.__file__).parent.parent / 'data/iris_l2_20150615_072426_3610091469_raster_t000_r00000.fits'
 
 
-def test_percentile_filter():
-    
-    data = astropy.io.fits.open(testfile)[4].data
-    # data = np.empty((1024, 1024, 1024), dtype=np.float32)
-
-    # print(data.shape)
-
-    ksz = (1, 25, 1)
-
-    fdata = despike.percentile_filter(data, 50, ksz)
-    
-    c = CubeSlicer(fdata)
-    plt.show()
-
-
 def test_identify():
 
     x = astropy.io.fits.open(testfile)[4].data
