- Fixing units.

kgpy/optics/system/rays.py
@@ -53,7 +53,7 @@ class Rays:
 
     @property
     def input_grid(self) -> tp.Tuple[u.Quantity, u.Quantity, u.Quantity]:
-        x, y, w = self.input_coordinates4
+        x, y, w = self.input_coordinates
         xx, yy, ww = np.meshgrid(x.value, y.value, w.value, indexing='ij')
         xx, yy, zz = xx << x.unit, yy << y.unit, ww << w.unit
         return xx, yy, zz