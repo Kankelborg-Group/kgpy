761e01855a4cf73be2d373b5acebe521a364acd6
Roy Smart <roytsmart@gmail.com>
2020-03-18 17:37:18 -0600

    - Coordinate systems   - More updates to propagate changes throughout the system.

--- kgpy/optics/zemax/system/surface/coordinate/tilt.py
+++ kgpy/optics/zemax/system/surface/coordinate/tilt.py
@@ -3,24 +3,27 @@ import dataclasses
 import typing as typ
 import astropy.units as u
 from kgpy.optics.system import coordinate
-from ... import configuration
 from .parent import ParentT, ParentBase
+from ... import configuration
 
-__all__ = ['Decenter']
+__all__ = ['Tilt']
 
 
 @dataclasses.dataclass
 class OperandBase:
     _x_op: configuration.SurfaceOperand = dataclasses.field(default=None, init=False, repr=False)
     _y_op: configuration.SurfaceOperand = dataclasses.field(default=None, init=False, repr=False)
+    _z_op: configuration.SurfaceOperand = dataclasses.field(default=None, init=False, repr=False)
+    _unit: typ.ClassVar[u.Unit] = u.deg
 
 
 @dataclasses.dataclass
-class Decenter(typ.Generic[ParentT], ParentBase[ParentT], coordinate.Decenter, OperandBase, ):
+class Tilt(typ.Generic[ParentT], ParentBase[ParentT], coordinate.Tilt, OperandBase, ):
 
     def _update(self) -> typ.NoReturn:
         self.x = self.x
         self.y = self.y
+        self.z = self.z
 
     @abc.abstractmethod
     def _x_setter(self, value: float):
@@ -28,6 +31,10 @@ class Decenter(typ.Generic[ParentT], ParentBase[ParentT], coordinate.Decenter, O
 
     @abc.abstractmethod
     def _y_setter(self, value: float):
+        pass
+
+    @abc.abstractmethod
+    def _z_setter(self, value: float):
         pass
 
     @property
@@ -39,7 +46,7 @@ class Decenter(typ.Generic[ParentT], ParentBase[ParentT], coordinate.Decenter, O
         self._x = value
         try:
             self._x_op.surface_index = self.parent.parent.lde_index
-            self.parent.parent.lde.system.set(value, self._x_setter, self._x_op, self.parent.parent.lens_units)
+            self.parent.parent.lde.system.set(value, self._x_setter, self._x_op, self._unit)
         except AttributeError:
             pass
 
@@ -52,7 +59,20 @@ class Decenter(typ.Generic[ParentT], ParentBase[ParentT], coordinate.Decenter, O
         self._y = value
         try:
             self._y_op.surface_index = self.parent.parent.lde_index
-            self.parent.parent.lde.system.set(value, self._y_setter, self._y_op, self.parent.parent.lens_units)
+            self.parent.parent.lde.system.set(value, self._y_setter, self._y_op, self._unit)
+        except AttributeError:
+            pass
+
+    @property
+    def z(self) -> u.Quantity:
+        return self._z
+
+    @z.setter
+    def z(self, value: u.Quantity):
+        self._z = value
+        try:
+            self._z_op.surface_index = self.parent.parent.lde_index
+            self.parent.parent.lde.system.set(value, self._z_setter, self._z_op, self._unit)
         except AttributeError:
             pass
 
