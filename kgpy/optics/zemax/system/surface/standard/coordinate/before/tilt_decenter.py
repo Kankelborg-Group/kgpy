cb3a68af7e1ab657c011c3b9f8084449e369c77e
Roy Smart <roytsmart@gmail.com>
2020-03-19 20:44:35 -0600

    - Make sure to correctly initialize overridden classes used in the  composition.

--- kgpy/optics/zemax/system/surface/standard/coordinate/before/tilt_decenter.py
+++ kgpy/optics/zemax/system/surface/standard/coordinate/before/tilt_decenter.py
@@ -1,8 +1,19 @@
+import dataclasses
 from ... import standard
 from .... import coordinate
+from . import Tilt, Decenter, TiltFirst
 
 __all__ = ['TiltDecenter']
 
 
-class TiltDecenter(coordinate.TiltDecenter[standard.Standard]):
+@dataclasses.dataclass
+class Base:
+
+    tilt: Tilt = dataclasses.field(default_factory=lambda: Tilt())
+    decenter: Decenter = dataclasses.field(default_factory=lambda: Decenter())
+    tilt_first: TiltFirst = dataclasses.field(default_factory=lambda: TiltFirst())
+
+
+@dataclasses.dataclass
+class TiltDecenter(coordinate.TiltDecenter[standard.Standard], Base):
     pass
