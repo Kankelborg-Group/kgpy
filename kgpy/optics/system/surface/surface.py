0dd17c655ee94bcc97b33bad8a9a76477ccd30df
Roy Smart <roytsmart@gmail.com>
2020-04-01 21:25:03 -0600

    - Refactored to `mixins.Broadcastable`.  - Removed `transform` private variable. `thickness` is now a fundamental attribute of the surface

--- kgpy/optics/system/surface/surface.py
+++ kgpy/optics/system/surface/surface.py
@@ -1,32 +1,22 @@
 import dataclasses
-import typing as typ
 import numpy as np
 import astropy.units as u
-
 from .. import mixin
-from . import coordinate
 
 __all__ = ['Surface']
 
 
 @dataclasses.dataclass
-class Base:
+class Surface(mixin.Broadcastable, mixin.Named):
     """
     This class represents a single optical surface. This class should be a drop-in replacement for a Zemax surface, and
     have all the same properties and behaviors.
     """
 
-    _transform: coordinate.Transform = dataclasses.field(default_factory=lambda: coordinate.Transform(), init=False,
-                                                         repr=False)
-
     thickness: u.Quantity = 0 * u.mm
     is_stop: bool = False
     is_active: 'np.ndarray[bool]' = np.array(True)
     is_visible: 'np.ndarray[bool]' = np.array(True)
-
-
-@dataclasses.dataclass
-class Surface(Base, mixin.ConfigBroadcast, mixin.Named):
 
     @property
     def config_broadcast(self):
@@ -35,14 +25,6 @@ class Surface(Base, mixin.ConfigBroadcast, mixin.Named):
             self.thickness,
             self.is_active,
         )
-
-    @property
-    def thickness(self) -> u.Quantity:
-        return self._transform.translate.z
-
-    @thickness.setter
-    def thickness(self, value: u.Quantity):
-        self._transform.translate.z = value
 
     @property
     def thickness_vector(self):
