4ba1c24cf9b987ce31b55933a7819bbbf4fb458a
Roy Smart <roytsmart@gmail.com>
2020-04-04 17:06:56 -0600

    - Changing `tilt_first` back to a boolean.

--- kgpy/optics/zemax/system/surface/standard/coordinate/after/__init__.py
+++ kgpy/optics/zemax/system/surface/standard/coordinate/after/__init__.py
@@ -1,4 +1,3 @@
 from .tilt import Tilt
 from .decenter import Decenter
-from .tilt_first import TiltFirst
 from .tilt_decenter import TiltDecenter
--- kgpy/optics/zemax/system/surface/standard/coordinate/after/tilt_decenter.py
+++ kgpy/optics/zemax/system/surface/standard/coordinate/after/tilt_decenter.py
@@ -1,19 +1,26 @@
 import dataclasses
+from kgpy.optics.zemax import ZOSAPI
+from ..... import configuration
 from .... import coordinate
 from ... import standard
-from . import tilt as tilt_, decenter as decenter_, tilt_first as tilt_first_
+from . import tilt as tilt_, decenter as decenter_
 
 __all__ = ['TiltDecenter']
 
 
 @dataclasses.dataclass
-class Base:
+class TiltDecenter(coordinate.TiltDecenter[standard.Standard], ):
+
+    _tilt_first_op: configuration.SurfaceOperand = dataclasses.field(
+        default_factory=lambda: configuration.SurfaceOperand(
+            op_factory=lambda: ZOSAPI.Editors.MCE.MultiConfigOperandType.CAOR,
+        ),
+        init=False,
+        repr=False,
+    )
 
     tilt: tilt_.Tilt = dataclasses.field(default_factory=lambda: tilt_.Tilt())
     decenter: decenter_.Decenter = dataclasses.field(default_factory=lambda: decenter_.Decenter())
-    tilt_first: tilt_first_.TiltFirst = dataclasses.field(default_factory=lambda: tilt_first_.TiltFirst())
 
-
-@dataclasses.dataclass
-class TiltDecenter(coordinate.TiltDecenter[standard.Standard], Base):
-    pass
+    def _tilt_first_setter(self, value: int):
+        self.composite.lde_row.TiltDecenterData.AfterSurfaceOrder = value
