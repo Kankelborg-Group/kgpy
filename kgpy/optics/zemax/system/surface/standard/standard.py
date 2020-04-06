04ca85f3e261489113522995b207e2959386166a
Roy Smart <roytsmart@gmail.com>
2020-04-06 11:20:54 -0600

    - Added zemax links to attributes.  - Removed original function.

--- kgpy/optics/zemax/system/surface/standard/standard.py
+++ kgpy/optics/zemax/system/surface/standard/standard.py
@@ -1,121 +1,122 @@
 import dataclasses
 import typing as typ
 from astropy import units as u
-
 from kgpy.optics import system
 from kgpy.optics.zemax import ZOSAPI
-
-from ... import util, configuration
-# from .. import diffraction_grating, toroidal, aperture, material
-from .. import surface
+from ... import configuration
+from .. import surface, aperture as aperture_, material as material_
 from . import coordinate
 
-__all__ = ['Standard', 'add_to_zemax_system']
+__all__ = ['Standard']
+
+MaterialT = typ.TypeVar('MaterialT', bound=material_.Material)
+ApertureT = typ.TypeVar('ApertureT', bound=aperture_.Aperture)
 
 
+@dataclasses.dataclass
 class InstanceVarBase:
-
     _radius_op: configuration.SurfaceOperand = dataclasses.field(
         default_factory=lambda: configuration.SurfaceOperand(
-            op_type=ZOSAPI.Editors.MCE.MultiConfigOperandType.CRVT
+            op_factory=lambda: ZOSAPI.Editors.MCE.MultiConfigOperandType.CRVT
         ),
         init=None,
         repr=None,
     )
     _conic_op: configuration.SurfaceOperand = dataclasses.field(
         default_factory=lambda: configuration.SurfaceOperand(
-            op_type=ZOSAPI.Editors.MCE.MultiConfigOperandType.CONN
+            op_factory=lambda: ZOSAPI.Editors.MCE.MultiConfigOperandType.CONN
         ),
         init=None,
         repr=None,
     )
 
 
-
-class Standard(system.surface.Standard, surface.Surface):
-    pass
-
-
-def add_to_zemax_system(
-        zemax_system: ZOSAPI.IOpticalSystem,
-        surface: 'system.surface.Standard',
-        surface_index: int,
-        configuration_shape: typ.Tuple[int],
-        zemax_units: u.Unit,
-):
-
-    op_radius = ZOSAPI.Editors.MCE.MultiConfigOperandType.CRVT
-    op_conic = ZOSAPI.Editors.MCE.MultiConfigOperandType.CONN
-    op_decenter_before_x = ZOSAPI.Editors.MCE.MultiConfigOperandType.CBDX
-    op_decenter_before_y = ZOSAPI.Editors.MCE.MultiConfigOperandType.CBDY
-    op_tilt_before_x = ZOSAPI.Editors.MCE.MultiConfigOperandType.CBTX
-    op_tilt_before_y = ZOSAPI.Editors.MCE.MultiConfigOperandType.CBTY
-    op_tilt_before_z = ZOSAPI.Editors.MCE.MultiConfigOperandType.CBTZ
-    op_order_before = ZOSAPI.Editors.MCE.MultiConfigOperandType.CBOR
-    op_decenter_after_x = ZOSAPI.Editors.MCE.MultiConfigOperandType.CADX
-    op_decenter_after_y = ZOSAPI.Editors.MCE.MultiConfigOperandType.CADY
-    op_tilt_after_x = ZOSAPI.Editors.MCE.MultiConfigOperandType.CATX
-    op_tilt_after_y = ZOSAPI.Editors.MCE.MultiConfigOperandType.CATY
-    op_tilt_after_z = ZOSAPI.Editors.MCE.MultiConfigOperandType.CATZ
-    op_order_after = ZOSAPI.Editors.MCE.MultiConfigOperandType.CAOR
-
-    unit_radius = 1 / zemax_units
-    unit_conic = u.dimensionless_unscaled
-    unit_decenter_before_x = zemax_units
-    unit_decenter_before_y = zemax_units
-    unit_tilt_before_x = u.deg
-    unit_tilt_before_y = u.deg
-    unit_tilt_before_z = u.deg
-    unit_order_before = None
-    unit_decenter_after_x = zemax_units
-    unit_decenter_after_y = zemax_units
-    unit_tilt_after_x = u.deg
-    unit_tilt_after_y = u.deg
-    unit_tilt_after_z = u.deg
-    unit_order_after = None
-
-    util.set_float(zemax_system, 1 / surface.radius, configuration_shape, op_radius, unit_radius, surface_index)
-
-    util.set_float(zemax_system, surface.conic, configuration_shape, op_conic, unit_conic, surface_index)
-
-    material.add_to_zemax_surface(zemax_system, surface.material, surface_index, configuration_shape, zemax_units)
-
-    aperture.add_to_zemax_surface(zemax_system, surface.aperture, surface_index, configuration_shape, zemax_units)
-
-    util.set_float(zemax_system, surface.transform_before.decenter.x, configuration_shape, op_decenter_before_x,
-                   unit_decenter_before_x, surface_index)
-    util.set_float(zemax_system, surface.transform_before.decenter.y, configuration_shape, op_decenter_before_y,
-                   unit_decenter_before_y, surface_index)
-    util.set_float(zemax_system, surface.transform_before.tilt.x, configuration_shape, op_tilt_before_x,
-                   unit_tilt_before_x, surface_index)
-    util.set_float(zemax_system, surface.transform_before.tilt.y, configuration_shape, op_tilt_before_y,
-                   unit_tilt_before_y, surface_index)
-    util.set_float(zemax_system, surface.transform_before.tilt.z, configuration_shape, op_tilt_before_z,
-                   unit_tilt_before_z, surface_index)
-    util.set_float(zemax_system, float(surface.transform_before.tilt_first.value), configuration_shape, op_order_before,
-                   unit_order_before, surface_index)
-
-    util.set_float(zemax_system, surface.transform_after.decenter.x, configuration_shape, op_decenter_after_x,
-                   unit_decenter_after_x, surface_index)
-    util.set_float(zemax_system, surface.transform_after.decenter.y, configuration_shape, op_decenter_after_y,
-                   unit_decenter_after_y, surface_index)
-    util.set_float(zemax_system, surface.transform_after.tilt.x, configuration_shape, op_tilt_after_x,
-                   unit_tilt_after_x, surface_index)
-    util.set_float(zemax_system, surface.transform_after.tilt.y, configuration_shape, op_tilt_after_y,
-                   unit_tilt_after_y, surface_index)
-    util.set_float(zemax_system, surface.transform_after.tilt.z, configuration_shape, op_tilt_after_z,
-                   unit_tilt_after_z, surface_index)
-    util.set_float(zemax_system, float(not surface.transform_after.tilt_first.value), configuration_shape,
-                   op_order_after, unit_order_after, surface_index)
-
-    if isinstance(surface, system.surface.DiffractionGrating):
-        diffraction_grating.add_to_zemax_system(zemax_system, surface, surface_index, configuration_shape, zemax_units)
-
-    elif isinstance(surface, system.surface.Toroidal):
-        toroidal.add_to_zemax_system(zemax_system, surface, surface_index, configuration_shape, zemax_units)
+@dataclasses.dataclass
+class Base(typ.Generic[MaterialT, ApertureT]):
+    material: MaterialT = dataclasses.field(default_factory=material_.NoMaterial(), init=False, repr=False)
+    aperture: ApertureT = dataclasses.field(default_factory=aperture_.NoAperture(), init=False, repr=False)
+    transform_before: coordinate.before.TiltDecenter = dataclasses.field(
+        default_factory=coordinate.before.TiltDecenter(), init=False, repr=False
+    )
+    transform_after: coordinate.after.TiltDecenter = dataclasses.field(
+        default_factory=coordinate.after.TiltDecenter(), init=False, repr=False
+    )
 
 
+class Standard(Base[MaterialT, ApertureT], system.surface.Standard, InstanceVarBase, surface.Surface,
+               typ.Generic[MaterialT, ApertureT]):
 
+    def _update(self) -> typ.NoReturn:
+        super()._update()
+        self.radius = self.radius
+        self.conic = self.conic
+        self.material = self.material
+        self.aperture = self.aperture
+        self.transform_before = self.transform_before
+        self.transform_after = self.transform_after
 
+    def _radius_setter(self, value: float):
+        self.lde_row.Radius = value
 
+    @property
+    def radius(self) -> u.Quantity:
+        return self._radius
 
+    @radius.setter
+    def radius(self, value: u.Quantity):
+        self._radius = value
+        try:
+            self.set(value, self._radius_setter, self._radius_op, self.lens_units)
+        except AttributeError:
+            pass
+
+    def _conic_setter(self, value: float):
+        self.lde_row.Conic = value
+
+    @property
+    def conic(self) -> u.Quantity:
+        return self._conic
+
+    @conic.setter
+    def conic(self, value: u.Quantity):
+        self._conic = value
+        try:
+            self.set(value, self._conic_setter, self._conic_op)
+        except AttributeError:
+            pass
+
+    @property
+    def material(self) -> MaterialT:
+        return self._material
+
+    @material.setter
+    def material(self, value: MaterialT):
+        value._composite = self
+        self._material = value
+
+    @property
+    def aperture(self) -> ApertureT:
+        return self._aperture
+
+    @aperture.setter
+    def aperture(self, value: ApertureT):
+        value._composite = self
+        self._aperture = value
+
+    @property
+    def transform_before(self) -> coordinate.before.TiltDecenter:
+        return self._transform_before
+
+    @transform_before.setter
+    def transform_before(self, value: coordinate.before.TiltDecenter):
+        value._composite = self
+        self._transform_before = value
+
+    @property
+    def transform_after(self) -> coordinate.after.TiltDecenter:
+        return self._transform_after
+
+    @transform_after.setter
+    def transform_after(self, value: coordinate.after.TiltDecenter):
+        value._composite = self
+        self._transform_after = value
