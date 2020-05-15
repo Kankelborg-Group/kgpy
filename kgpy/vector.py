c9d6afc521338e412fff796153debafcdfa42d61
Roy Smart <roytsmart@gmail.com>
2020-05-14 22:18:49 -0600

    - Added small module to help with vector manipulations.

---
+++ kgpy/vector.py
@@ -0,0 +1,61 @@
+import typing as typ
+import numpy as np
+import astropy.units as u
+
+__all__ = ['dot', 'length', 'normalize']
+
+
+@typ.overload
+def dot(a: np.ndarray, b: np.ndarray, keepdims: bool = True) -> np.ndarray:
+    ...
+
+
+@typ.overload
+def dot(a: u.Quantity, b: u.Quantity, keepdims: bool = True) -> u.Quantity:
+    ...
+
+
+def dot(a, b, keepdims=True):
+    return np.sum(a * b, axis=~0, keepdims=keepdims)
+
+
+@typ.overload
+def length(a: np.ndarray, keepdims: bool = True) -> np.ndarray:
+    ...
+
+
+@typ.overload
+def length(a: u.Quantity, keepdims: bool = True) -> u.Quantity:
+    ...
+
+
+def length(a, keepdims=True):
+    return np.sqrt(np.sum(np.square(a), axis=~0, keepdims=keepdims))
+
+
+@typ.overload
+def normalize(a: np.ndarray, keepdims: bool = True) -> np.ndarray:
+    ...
+
+
+@typ.overload
+def normalize(a: u.Quantity, keepdims: bool = True) -> u.Quantity:
+    ...
+
+
+def normalize(a, keepdims=True):
+    return a / length(a, keepdims=keepdims)
+
+
+@typ.overload
+def from_components(x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
+    ...
+
+
+@typ.overload
+def from_components(x: u.Quantity, y: u.Quantity, z: u.Quantity) -> u.Quantity:
+    ...
+
+
+def from_components(x, y, z):
+    return np.stack([x, y, z], axis=~0)
