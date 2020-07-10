12e1f32b295901fc7fc8a9e6305a2bd7e7d93847
byrdie <roytsmart@gmail.com>
2020-07-10 10:56:48 -0600

    - Removed TransformedSubstrate class.

--- kgpy/optics/surface/__init__.py
+++ kgpy/optics/surface/__init__.py
@@ -10,4 +10,3 @@ from .elliptical_grating import EllipticalGrating1
 from .coordinate_transform import CoordinateTransform
 from .transformed import Transformed
 from .substrate import Substrate
-from .transformed_substrate import TransformedSubstrate
