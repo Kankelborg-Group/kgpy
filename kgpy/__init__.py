99683895592781c353b3ae440b6ecaced1694b70
Roy Smart <roytsmart@gmail.com>
2020-07-30 15:21:43 -0600

    - Try without error on warnings for a bit.

--- kgpy/__init__.py
+++ kgpy/__init__.py
@@ -1,3 +1,9 @@
+"""
+kgpy base package
+"""
+
+__all__ = ['linspace', 'midspace', 'Name', 'Component']
+
 from .linspace import linspace, midspace
 from .name import Name
 from .component import Component
