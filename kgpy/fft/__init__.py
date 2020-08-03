88ff1500d753ed43d9c53475bdf671ebd3c6a435
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2020-08-03 15:14:49 -0600

    Refactoring and porting more from IDL to Python

--- kgpy/fft/__init__.py
+++ kgpy/fft/__init__.py
@@ -1,1 +1,15 @@
-from . import powerspec
+"""
+`kgpy.fft` is a module of code ported from IDL programs originally written by Charles. The main function of the programs
+here is to build arrays for power spectra analysis using FFTs.
+"""
+__all__ = [
+    'power_law_image',
+    'periodogram_fft',
+    'k_arr',
+    'k_arr2d',
+    'spec2d'
+]
+
+from .random import power_law_image
+from .freq import k_arr, k_arr2d
+from .power import periodogram_fft, spec2d, spec, spec_xy
--- kgpy/fft/test_powerspec.py
+++ kgpy/fft/test_powerspec.py
@@ -2,22 +2,25 @@ import numpy as np
 import scipy.ndimage
 import matplotlib.pyplot as plt
 
-from kgpy.fft import powerspec
+import kgpy.fft.freq
+import kgpy.fft.power
+from kgpy.fft import random
 
 if __name__ == '__main__':
     angle = 43
-    nx = 2048
+    nx = 20
     ny = nx
-    nfreq = 64
+    nfreq = 4
 
-    img = powerspec.random_image(nx, ny, 0, 100, 2)
-    k, _, _, _, _ = powerspec.k_arr2d(nx, ny)
+    img = random.power_law_image(nx, ny, 0, 100, 2)
+    k, _, _, _, _ = kgpy.fft.freq.k_arr2d(nx, ny)
+    print(k)
 
-    spec_img = powerspec.powerspec2d(img, nfreq, retain_dc=False)
+    spec_img = kgpy.fft.power.spec2d(img, nfreq, retain_dc=False)
 
     rot = scipy.ndimage.rotate(img, angle=angle, reshape=False, prefilter=True, order=3)
     rot = scipy.ndimage.rotate(rot, angle=-angle, reshape=False, prefilter=True, order=3)
-    spec_rot = powerspec.powerspec2d(rot, nfreq, retain_dc=False)
+    spec_rot = kgpy.fft.power.spec2d(rot, nfreq, retain_dc=False)
 
     # ToDo, convert this to histogram
 
