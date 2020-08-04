9a18cf290136e95406f840e772c12f5adf7a2bdd
Nelson Goldsworth <nelson.goldsworth@gmail.com>
2020-08-03 20:29:37 -0600

    Refactoring and porting more from IDL to Python

--- kgpy/fft/__init__.py
+++ kgpy/fft/__init__.py
@@ -3,13 +3,15 @@
 here is to build arrays for power spectra analysis using FFTs.
 """
 __all__ = [
-    'power_law_image',
-    'periodogram_fft',
     'k_arr',
     'k_arr2d',
-    'spec2d'
+    'periodogram_fft',
+    'spec2d',
+    'spec_xy',
+    'spec',
+    'power_law_image',
 ]
 
-from .random import power_law_image
 from .freq import k_arr, k_arr2d
 from .power import periodogram_fft, spec2d, spec, spec_xy
+from .random import power_law_image
