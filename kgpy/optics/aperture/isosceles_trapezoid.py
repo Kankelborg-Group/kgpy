7f26a53e68960d5c1f819b92fb5c6580ed1cd1bf
Roy Smart <roytsmart@gmail.com>
2020-08-24 14:35:42 -0600

    - Added copy method.

--- kgpy/optics/aperture/isosceles_trapezoid.py
+++ kgpy/optics/aperture/isosceles_trapezoid.py
@@ -31,3 +31,15 @@ class IsoscelesTrapezoid(Polygon):
             self.outer_radius,
             self.wedge_half_angle,
         )
+
+    def copy(self) -> 'IsoscelesTrapezoid':
+        return IsoscelesTrapezoid(
+            num_samples=self.num_samples,
+            is_active=self.is_active,
+            is_test_stop=self.is_test_stop,
+            is_obscuration=self.is_obscuration,
+            decenter=self.decenter.copy(),
+            inner_radius=self.inner_radius.copy(),
+            outer_radius=self.outer_radius.copy(),
+            wedge_half_angle=self.wedge_half_angle.copy(),
+        )
