502dde56706de286011bfb8a3b0db08a2c515e02
Roy Smart <roytsmart@gmail.com>
2020-09-07 12:38:00 -0600

    Let the scientific notation state be detected automatically.

--- kgpy/format.py
+++ kgpy/format.py
@@ -1,3 +1,5 @@
+import typing as typ
+import numpy as np
 import astropy.units as u
 
 __all__ = ['quantity']
@@ -5,11 +7,17 @@ __all__ = ['quantity']
 
 def quantity(
         a: u.Quantity,
-        scientific_notation: bool = False,
+        scientific_notation: typ.Optional[bool] = None,
         digits_after_decimal: int = 3
 ) -> str:
     estr = '{0.value:0.' + str(digits_after_decimal) + 'e} {0.unit:latex}'
     fstr = '{0.value:0.' + str(digits_after_decimal) + 'f} {0.unit:latex}'
+
+    if scientific_notation is None:
+        if np.abs(a.value) > 0.1:
+            scientific_notation = False
+        else:
+            scientific_notation = True
 
     if not scientific_notation:
         return fstr.format(a)
