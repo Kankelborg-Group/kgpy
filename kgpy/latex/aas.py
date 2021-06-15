233f90fa4a73908d5809e3fd8cd921dfec8554c0
Roy Smart <roytsmart@gmail.com>
2021-06-15 16:35:30 -0600

    Added Fig and Gridline classes to the kgpy.latex.aas module.

--- kgpy/latex/aas.py
+++ kgpy/latex/aas.py
@@ -1,6 +1,8 @@
+import pathlib
+import typing as typ
 import dataclasses
 import pylatex
-from . import Author as AuthorBase
+from . import Author as AuthorBase, textwidth
 
 __all__ = ['Affiliation']
 
@@ -20,5 +22,33 @@ class Author(AuthorBase):
     def dumps(self):
         return super().dumps() + self.affilation.dumps()
 
+
+@dataclasses.dataclass
+class Fig(pylatex.base_classes.LatexObject):
+    _command_str: typ.ClassVar[str] = 'fig'
+    file: pathlib.Path
+    width: float
+    caption: str
+
+    def dumps(self) -> str:
+        return pylatex.Command(self._command_str, arguments=[pylatex.NoEscape(self.file.as_posix()), self.width, self.caption]).dumps()
+
+
+@dataclasses.dataclass
+class LeftFig(Fig):
+    _command_str: typ.ClassVar[str] = 'leftfig'
+
+
+@dataclasses.dataclass
+class RightFig(Fig):
+    _command_str: typ.ClassVar[str] = 'rightfig'
+
+
+@dataclasses.dataclass
+class Gridline(pylatex.base_classes.LatexObject):
+    figures: typ.List[Fig]
+
+    def dumps(self) -> str:
+        return pylatex.Command('gridline', arguments=pylatex.NoEscape('\n'.join([f.dumps() for f in self.figures]))).dumps()
 
 
