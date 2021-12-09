93c3bab762bbb48e5802dd189b7c4e299bb34df2
Roy Smart <roytsmart@gmail.com>
2021-12-09 16:34:23 -0700

    Moved kgpy.mixin.SaveableAsDxf class to kgpy.dxf.WritableMixin class to avoid circular references with kgpy.transform.rigid.Transform

---
+++ kgpy/dxf.py
@@ -0,0 +1,22 @@
+import typing as typ
+import abc
+import dataclasses
+import astropy.units as u
+from ezdxf.addons.r12writer import R12FastStreamWriter
+import kgpy.transform
+
+
+WritableMixinT = typ.TypeVar('WritableMixinT', bound='WritableMixin')
+
+
+@dataclasses.dataclass
+class WritableMixin(abc.ABC):
+
+    @abc.abstractmethod
+    def write_to_dxf(
+            self: WritableMixinT,
+            file_writer: R12FastStreamWriter,
+            unit: u.Unit,
+            transform_extra: typ.Optional[kgpy.transform.rigid.Transform],
+    ) -> None:
+        pass
