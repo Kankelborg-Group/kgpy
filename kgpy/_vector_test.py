6b8b0a2568609eceb141a858ed19ce2f964e9f12
Roy Smart <roytsmart@gmail.com>
2022-01-17 20:42:17 -0700

    Improved kgpy._vector_test.TestCartesian2D.test__mul__() method.

--- kgpy/_vector_test.py
+++ kgpy/_vector_test.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import astropy.units as u
 import kgpy.labeled
 import kgpy.uncertainty
@@ -17,12 +18,17 @@ class TestCartesian2D:
         kgpy.labeled.LinearSpace(4, 5, 11, axis='b'),
         kgpy.labeled.LinearSpace(4, 5, 11, axis='b') * u.mm,
         kgpy.uncertainty.Uniform(4, 2),
+        kgpy.vector.Cartesian2D(3, 4),
     ])
     def test__mul__(self, a, b):
         c = a * b
         d = b * a
-        assert isinstance(c, kgpy.vector.AbstractVector)
-        assert isinstance(d, kgpy.vector.AbstractVector)
-
-        print(c)
-        print(d)
+        assert isinstance(c, kgpy.vector.Cartesian2D)
+        assert isinstance(d, kgpy.vector.Cartesian2D)
+        assert np.all(c == d)
+        if not isinstance(b, kgpy.vector.Cartesian2D):
+            assert np.all(c.x == a.x * b)
+            assert np.all(c.y == a.y * b)
+        else:
+            assert np.all(c.x == a.x * b.x)
+            assert np.all(c.y == a.y * b.y)
