71503b62967f0612139ffa69c2c7517f8f9a7a66
Roy Smart <roytsmart@gmail.com>
2022-01-18 21:35:53 -0700

    Wrote some new tests for kgpy.matrix.Cartesian2D in kgpy._matrix_test module.

--- kgpy/_matrix_test.py
+++ kgpy/_matrix_test.py
@@ -1,81 +1,62 @@
 import pytest
-import astropy.units as u
-from . import matrix
-from .matrix import xx, xy, yx, yy
+import kgpy.vector
+import kgpy.matrix
 
-magic1 = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]] * u.dimensionless_unscaled
-
-magic2 = [[10, 11, 12],
-          [13, 14, 15],
-          [16, 17, 18]] * u.dimensionless_unscaled
-
-magic3 = [[19, 20, 21],
-          [22, 23, 24],
-          [25, 26, 27]] * u.dimensionless_unscaled
-
-test_matrices = [magic1, magic2, magic3]
-
-
-@pytest.mark.parametrize(
-    argnames='mat',
-    argvalues=[[a] for a in test_matrices],
+identity = kgpy.matrix.Cartesian2D.identity()
+xx = kgpy.matrix.Cartesian2D(
+    x=kgpy.vector.Cartesian2D(x=1),
+    y=kgpy.vector.Cartesian2D(),
 )
-def test_transpose_involution(mat: u.Quantity):
-    b = matrix.transpose(matrix.transpose(mat))
-    assert (b == mat).all()
-
-
-@pytest.mark.parametrize(
-    argnames='mat1, mat2',
-    argvalues=[[a, b] for a in test_matrices for b in test_matrices]
+xy = kgpy.matrix.Cartesian2D(
+    x=kgpy.vector.Cartesian2D(y=1),
+    y=kgpy.vector.Cartesian2D(),
 )
-def test_transpose_addition(mat1: u.Quantity, mat2: u.Quantity):
-    b = matrix.transpose(mat1 + mat2)
-    c = matrix.transpose(mat1) + matrix.transpose(mat2)
-    assert (b == c).all()
-
-
-@pytest.mark.parametrize(
-    argnames='mat1, mat2',
-    argvalues=[[a, b] for a in test_matrices for b in test_matrices]
+yx = kgpy.matrix.Cartesian2D(
+    x=kgpy.vector.Cartesian2D(),
+    y=kgpy.vector.Cartesian2D(x=1),
 )
-def test_transpose_multiplication(mat1: u.Quantity, mat2: u.Quantity):
-    b = matrix.transpose(matrix.mul(mat1, mat2))
-    c = matrix.mul(matrix.transpose(mat2), matrix.transpose(mat1))
-    assert (b == c).all()
-
-
-@pytest.mark.parametrize(
-    argnames='mat1, mat2, mat_out',
-    argvalues=[
-        [xx, xx, xx],
-        [xy, yx, xx],
-        [yx, xy, yy],
-        [yy, yy, yy],
-    ],
+test_1 = kgpy.matrix.Cartesian2D(
+    x=kgpy.vector.Cartesian2D(x=1, y=2),
+    y=kgpy.vector.Cartesian2D(x=3, y=4),
 )
-def test_mul(mat1: u.Quantity, mat2: u.Quantity, mat_out: u.Quantity):
-    b = matrix.mul(mat1, mat2)
-    assert (b == mat_out).all()
-
-
-@pytest.mark.parametrize(
-    argnames='mat1, mat2, mat3',
-    argvalues=[[a, b, c] for a in test_matrices for b in test_matrices for c in test_matrices]
+test_2 = kgpy.matrix.Cartesian2D(
+    x=kgpy.vector.Cartesian2D(x=5, y=6),
+    y=kgpy.vector.Cartesian2D(x=7, y=8),
 )
-def test_mul_distributivity(mat1: u.Quantity, mat2: u.Quantity, mat3: u.Quantity):
-    b = matrix.mul(mat1, mat2 + mat3)
-    c = matrix.mul(mat1, mat2) + matrix.mul(mat1, mat3)
-    assert (b == c).all()
 
 
-@pytest.mark.parametrize(
-    argnames='mat1, mat2, mat3',
-    argvalues=[[a, b, c] for a in test_matrices for b in test_matrices for c in test_matrices]
-)
-def test_mul_associativity(mat1: u.Quantity, mat2: u.Quantity, mat3: u.Quantity):
-    b = matrix.mul(matrix.mul(mat1, mat2), mat3)
-    c = matrix.mul(mat1, matrix.mul(mat2, mat3))
-    assert (b == c).all()
+class TestCartesian2D:
+
+    @pytest.mark.parametrize(
+        argnames='a',
+        argvalues=[test_1, test_2],
+    )
+    def test_transpose(self, a: kgpy.matrix.Cartesian2D):
+        assert a == a.transpose.transpose
+
+    @pytest.mark.parametrize(
+        argnames='a,b,out',
+        argvalues=[
+            (identity, identity, identity, ),
+            (xy, yx, xx),
+        ]
+    )
+    def test__matmul__(
+            self,
+            a: kgpy.matrix.Cartesian2D,
+            b: kgpy.matrix.Cartesian2D,
+            out: kgpy.matrix.Cartesian2D,
+    ):
+        out_test = a @ b
+        assert out_test == out
+
+    @pytest.mark.parametrize(
+        argnames='a',
+        argvalues=[test_1, test_2],
+    )
+    def test__invert__(self, a: kgpy.matrix.Cartesian2D):
+        b = ~a
+        c = ~b
+        assert a @ b == identity
+        assert c == a
+        assert ~a.transpose == b.transpose
