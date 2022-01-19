b591d0a7001adad4bb9a4a03e0a9ce7d7215e9f0
Roy Smart <roytsmart@gmail.com>
2022-01-18 21:59:30 -0700

    Removed deprecated code in kgpy.matrix module.

--- kgpy/matrix.py
+++ kgpy/matrix.py
@@ -16,60 +16,6 @@ AbstractMatrixT = typ.TypeVar('AbstractMatrixT', bound='AbstractMatrix')
 Cartesian2DT = typ.TypeVar('Cartesian2DT', bound='Cartesian2D')
 Cartesian3DT = typ.TypeVar('Cartesian3DT', bound='Cartesian3D')
 
-# __all__ = [
-#     'xx', 'xy', 'xz',
-#     'yx', 'yy', 'yz',
-#     'zx', 'zy', 'zz',
-#     'mul'
-# ]
-#
-#
-# def transpose(a: u.Quantity) -> u.Quantity:
-#     return np.swapaxes(a, ~0, ~1)
-#
-#
-# def mul(a: u.Quantity, b: u.Quantity) -> u.Quantity:
-#     a = np.expand_dims(a, ~0)
-#     b = np.expand_dims(b, ~2)
-#     return np.sum(a * b, axis=~1)
-#
-#
-# xx = [[1, 0, 0],
-#       [0, 0, 0],
-#       [0, 0, 0]] * u.dimensionless_unscaled
-#
-# xy = [[0, 1, 0],
-#       [0, 0, 0],
-#       [0, 0, 0]] * u.dimensionless_unscaled
-#
-# xz = [[0, 0, 1],
-#       [0, 0, 0],
-#       [0, 0, 0]] * u.dimensionless_unscaled
-#
-# yx = [[0, 0, 0],
-#       [1, 0, 0],
-#       [0, 0, 0]] * u.dimensionless_unscaled
-#
-# yy = [[0, 0, 0],
-#       [0, 1, 0],
-#       [0, 0, 0]] * u.dimensionless_unscaled
-#
-# yz = [[0, 0, 0],
-#       [0, 0, 1],
-#       [0, 0, 0]] * u.dimensionless_unscaled
-#
-# zx = [[0, 0, 0],
-#       [0, 0, 0],
-#       [1, 0, 0]] * u.dimensionless_unscaled
-#
-# zy = [[0, 0, 0],
-#       [0, 0, 0],
-#       [0, 1, 0]] * u.dimensionless_unscaled
-#
-# zz = [[0, 0, 0],
-#       [0, 0, 0],
-#       [0, 0, 1]] * u.dimensionless_unscaled
-
 
 @dataclasses.dataclass(eq=False)
 class AbstractMatrix(
