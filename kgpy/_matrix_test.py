09fa6e7ccfae469a45ad72d5cdfcda7e9fef14a5
Roy Smart <roytsmart@gmail.com>
2022-02-07 13:27:16 -0700

    Rewrote `kgpy._matrix_test` module to use class-level parameterization.

--- kgpy/_matrix_test.py
+++ kgpy/_matrix_test.py
@@ -1,64 +1,119 @@
 import pytest
+import numpy as np
+import astropy.units as u
+import kgpy.labeled
+import kgpy.uncertainty
 import kgpy.vector
 import kgpy.matrix
 
-identity = kgpy.matrix.Cartesian2D.identity()
-xx = kgpy.matrix.Cartesian2D(
-    x=kgpy.vector.Cartesian2D(x=1),
-    y=kgpy.vector.Cartesian2D(),
-)
-xy = kgpy.matrix.Cartesian2D(
-    x=kgpy.vector.Cartesian2D(y=1),
-    y=kgpy.vector.Cartesian2D(),
-)
-yx = kgpy.matrix.Cartesian2D(
-    x=kgpy.vector.Cartesian2D(),
-    y=kgpy.vector.Cartesian2D(x=1),
-)
-test_1 = kgpy.matrix.Cartesian2D(
-    x=kgpy.vector.Cartesian2D(x=1, y=2),
-    y=kgpy.vector.Cartesian2D(x=3, y=4),
-)
-test_2 = kgpy.matrix.Cartesian2D(
-    x=kgpy.vector.Cartesian2D(x=5, y=6),
-    y=kgpy.vector.Cartesian2D(x=7, y=8),
-)
 
-
+@pytest.mark.parametrize(
+    argnames='xy',
+    argvalues=[
+        2,
+        kgpy.labeled.LinearSpace(2, 3, 3, axis='x')
+    ],
+)
+@pytest.mark.parametrize(
+    argnames='yx',
+    argvalues=[
+        3,
+        kgpy.labeled.LinearSpace(3, 4, 4, axis='y'),
+    ],
+)
+@pytest.mark.parametrize(
+    argnames='width_xy',
+    argvalues=[
+        None,
+        1,
+    ],
+)
+@pytest.mark.parametrize(
+    argnames='width_yx',
+    argvalues=[
+        None,
+        1,
+    ],
+)
+@pytest.mark.parametrize(
+    argnames='unit',
+    argvalues=[
+        1,
+        u.mm,
+    ],
+)
 class TestCartesian2D:
 
-    @pytest.mark.parametrize(
-        argnames='a',
-        argvalues=[test_1, test_2],
-    )
-    def test_transpose(self, a: kgpy.matrix.Cartesian2D):
-        assert a == a.transpose.transpose
+    def _calc_matrix(
+            self,
+            xy: kgpy.labeled.ArrayLike,
+            yx: kgpy.labeled.ArrayLike,
+            width_xy: kgpy.labeled.ArrayLike,
+            width_yx: kgpy.labeled.ArrayLike,
+            unit: u.Unit,
+    ):
+        if width_xy is not None:
+            xy = kgpy.uncertainty.Uniform(xy, width=width_xy)
+        if width_yx is not None:
+            yx = kgpy.uncertainty.Uniform(yx, width=width_yx)
+        a = kgpy.matrix.Cartesian2D(
+            x=kgpy.vector.Cartesian2D(x=kgpy.labeled.Array(1), y=xy),
+            y=kgpy.vector.Cartesian2D(x=yx, y=kgpy.labeled.Array(4)),
+        )
+        return a * unit
 
-    @pytest.mark.parametrize(
-        argnames='a,b,out',
-        argvalues=[
-            (identity, kgpy.vector.Cartesian2D.x_hat(), kgpy.vector.Cartesian2D.x_hat()),
-            (identity, identity, identity, ),
-            (xy, yx, xx),
-        ]
-    )
+    def test_transpose(
+            self,
+            xy: kgpy.labeled.ArrayLike,
+            yx: kgpy.labeled.ArrayLike,
+            width_xy: kgpy.labeled.ArrayLike,
+            width_yx: kgpy.labeled.ArrayLike,
+            unit: u.Unit,
+    ):
+        a = self._calc_matrix(xy, yx, width_xy, width_yx, unit)
+        assert not np.all(a == a.transpose)
+        assert np.all(a == a.transpose.transpose)
+
     def test__matmul__(
             self,
-            a: kgpy.matrix.Cartesian2D,
-            b: kgpy.matrix.Cartesian2D,
-            out: kgpy.matrix.Cartesian2D,
+            xy: kgpy.labeled.ArrayLike,
+            yx: kgpy.labeled.ArrayLike,
+            width_xy: kgpy.labeled.ArrayLike,
+            width_yx: kgpy.labeled.ArrayLike,
+            unit: u.Unit,
     ):
-        out_test = a @ b
-        assert isinstance(out_test, type(out))
-        assert out_test == out
+        a = self._calc_matrix(xy, yx, width_xy, width_yx, unit)
+        assert np.all(a @ (a + a) == a @ a + a @ a)
+        assert np.all((a + a) @ a == a @ a + a @ a)
+        assert np.all((a @ a).transpose == a.transpose @ a.transpose)
 
-    @pytest.mark.parametrize(
-        argnames='a',
-        argvalues=[test_1, test_2],
-    )
-    def test__invert__(self, a: kgpy.matrix.Cartesian2D):
+    def test__inverse_numpy(
+            self,
+            xy: kgpy.labeled.ArrayLike,
+            yx: kgpy.labeled.ArrayLike,
+            width_xy: kgpy.labeled.ArrayLike,
+            width_yx: kgpy.labeled.ArrayLike,
+            unit: u.Unit,
+    ):
+        a = self._calc_matrix(xy, yx, width_xy, width_yx, unit)
+        b = a.inverse_numpy()
+        print(b)
+        c = b.inverse_numpy()
+        assert np.all(np.isclose(a @ b, kgpy.matrix.Cartesian2D.identity()))
+        assert np.all(np.isclose(c, a))
+        assert np.all(np.isclose(~a.transpose, b.transpose))
+
+    def test__invert__(
+            self,
+            xy: kgpy.labeled.ArrayLike,
+            yx: kgpy.labeled.ArrayLike,
+            width_xy: kgpy.labeled.ArrayLike,
+            width_yx: kgpy.labeled.ArrayLike,
+            unit: u.Unit,
+    ):
+        a = self._calc_matrix(xy, yx, width_xy, width_yx, unit)
         b = ~a
         c = ~b
-        assert a @ b == identity
-        assert c == a
-        assert ~a.transpose == b.transpose
+        assert np.all(np.isclose(a @ b, kgpy.matrix.Cartesian2D.identity()))
+        assert np.all(np.isclose(c, a))
+        assert np.all(np.isclose(~a.transpose, b.transpose))
