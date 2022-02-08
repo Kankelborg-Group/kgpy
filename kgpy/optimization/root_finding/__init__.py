dae5533ac6c9bb01a5fcbccb6737a9ece132d398
Roy Smart <roytsmart@gmail.com>
2022-02-07 17:16:51 -0700

    Added `kgpy.optimization.root_finding.secant()` function which combines the vector and scalar versions of the function.

--- kgpy/optimization/root_finding/__init__.py
+++ kgpy/optimization/root_finding/__init__.py
@@ -1,7 +1,80 @@
-"""
-A package for finding the roots of both scalar-valued and vector-valued functions/
-"""
+import typing as typ
+import numpy as np
+import kgpy.labeled
+import kgpy.vector
 
-__all__ = ['scalar', 'vector']
+InputT = typ.TypeVar('InputT', bound=kgpy.vector.VectorLike)
+OutputT = typ.TypeVar('OutputT', bound=kgpy.vector.VectorLike)
 
-from . import scalar, vector
+
+def secant(
+        func: typ.Callable[[InputT], OutputT],
+        root_guess: InputT,
+        step_size: InputT,
+        max_abs_error: OutputT,
+        max_iterations: int = 100,
+) -> InputT:
+
+    x0 = root_guess - step_size
+    x1 = root_guess
+
+    for i in range(max_iterations):
+
+        f1 = func(x1)
+
+        if isinstance(f1, kgpy.vector.AbstractVector):
+            root_error = f1.length
+        else:
+            root_error = np.abs(f1)
+
+        if np.all(root_error < max_abs_error):
+            print('max iteration', i)
+            return x1
+
+        dx = x1 - x0
+        if isinstance(dx, kgpy.vector.AbstractVector):
+            mask = dx.length != 0
+        else:
+            mask = dx != 0
+
+        jacobian = 0 * f1 / step_size
+
+        if isinstance(x1, kgpy.vector.AbstractVector):
+            for component in x1.coordinates:
+                x0_component = x1.copy_shallow()
+                setattr(x0_component, component, x0_component.coordinates[component] - step_size.coordinates[component])
+                df_component = f1 - func(x0_component)
+                dfdx = type(x0)()
+                setattr(dfdx, component, df_component / step_size.coordinates[component])
+                jacobian = jacobian + dfdx
+
+            if isinstance(f1, kgpy.vector.AbstractVector):
+                jacobian = jacobian.to_matrix()
+
+        else:
+            df = f1 - func(x0)
+            jacobian = jacobian + df / dx
+
+        if isinstance(f1, kgpy.vector.AbstractVector) and isinstance(x1, kgpy.vector.AbstractVector):
+            correction = 0.9999 * ~jacobian @ f1
+
+        else:
+            correction = 0.9999 * f1 / jacobian
+
+        x2 = x1 - correction
+
+        if not isinstance(x2, (int, float, complex,)) and x2.shape:
+            if not isinstance(mask, kgpy.labeled.ArrayInterface):
+                mask = kgpy.labeled.Array(mask)
+            mask = np.broadcast_to(mask, x2.shape)
+            x2 = np.broadcast_to(x2, x2.shape).copy()
+            if i == 0:
+                x1 = x1 + 0 * x2
+            x2[~mask] = x1[~mask]
+
+        x0 = x1
+        x1 = x2
+
+    raise ValueError('Max iterations exceeded')
+
+
---
+++ kgpy/optimization/root_finding/__init__test.py
@@ -0,0 +1,69 @@
+import pytest
+import numpy as np
+import astropy.units as u
+import kgpy.labeled
+import kgpy.uncertainty
+import kgpy.vector
+from . import secant
+
+
+@pytest.mark.parametrize(
+    argnames='root_x',
+    argvalues=[
+        4,
+        kgpy.labeled.LinearSpace(4, 6, 3, axis='x'),
+    ]
+)
+@pytest.mark.parametrize(
+    argnames='root_y',
+    argvalues=[
+        None,
+        7,
+        kgpy.labeled.LinearSpace(7, 9, 3, axis='y'),
+    ]
+)
+@pytest.mark.parametrize(
+    argnames='width_x',
+    argvalues=[None, 1]
+)
+@pytest.mark.parametrize(
+    argnames='width_y',
+    argvalues=[None, 1]
+)
+@pytest.mark.parametrize(
+    argnames='unit',
+    argvalues=[1, u.mm]
+)
+def test_secant(
+        root_x: kgpy.labeled.ArrayLike,
+        root_y: kgpy.labeled.ArrayLike,
+        width_x: kgpy.labeled.ArrayLike,
+        width_y: kgpy.labeled.ArrayLike,
+        unit: u.Unit
+):
+    if width_x is not None:
+        root_x = kgpy.uncertainty.Uniform(root_x, width=width_x)
+
+    if width_y is not None:
+        if root_y is not None:
+            root_y = kgpy.uncertainty.Uniform(root_y, width=width_y)
+
+    if root_y is None:
+        root_1 = root_x
+    else:
+        root_1 = kgpy.vector.Cartesian2D(root_x, root_y)
+    root_1 = root_1 * unit
+    root_2 = 20 * unit
+
+    def func(position: kgpy.labeled.AbstractArray) -> kgpy.labeled.AbstractArray:
+        return (position - root_1) * (position - root_2)
+
+    root_guess = 0 * root_1
+    root = secant(
+        func=func,
+        root_guess=root_guess,
+        step_size=0.1 * unit + root_guess,
+        max_abs_error=1e-9 * unit * unit,
+    )
+
+    assert np.all(np.abs(root - root_1) < 1e-3 * unit)
