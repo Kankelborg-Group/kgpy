67687f08d76283d23991eed110a318dcf27f0112
Roy Smart <roytsmart@gmail.com>
2022-09-29 13:24:14 -0600

    Fixed bug in `kgpy.moment.percentile.argpercentile()`.

--- kgpy/moment/percentile.py
+++ kgpy/moment/percentile.py
@@ -5,7 +5,7 @@ __all__ = ['arg_percentile', 'intensity', 'shift', 'width', 'skew', 'first_four_
 
 
 def arg_percentile(cube: np.ndarray, percentile: float, axis: int = ~0) -> np.ndarray:
-    mod_axis = axis % cube.shape[axis]
+    mod_axis = axis % len(cube.shape)
 
     cs = np.cumsum(cube, axis=axis)
 
