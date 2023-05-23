d56a9b2096c429733442150d05c5eee838eb0da1
Roy Smart <roytsmart@gmail.com>
2023-05-23 09:16:16 -0600

    Greatly simplified the `kgpy.solar` module.

--- kgpy/solar.py
+++ kgpy/solar.py
@@ -1,11 +1,8 @@
 import typing as typ
 import dataclasses
-import pathlib
 import astropy.units as u
-import kgpy.chianti
-import kgpy.labeled
 import kgpy.uncertainty
-import kgpy.vectors
+import kgpy.optics
 import kgpy.function
 
 __all__ = ['angular_radius_max']
@@ -14,43 +11,22 @@ __all__ = ['angular_radius_max']
 angular_radius_max = (32 * u.arcmin + 32 * u.arcsec) / 2  # type: u.Quantity
 """maximum angular radius of the solar disk"""
 
-TimeT = typ.TypeVar('TimeT', bound=kgpy.uncertainty.ArrayLike)
-WavelengthT = typ.TypeVar('WavelengthT', bound=kgpy.uncertainty.ArrayLike)
-WavelengthReferenceT = typ.TypeVar('WavelengthReferenceT', bound=kgpy.uncertainty.ArrayLike)
-WavelengthRelativeT = typ.TypeVar('WavelengthRelativeT', bound=kgpy.uncertainty.ArrayLike)
-HelioprojectiveXT = typ.TypeVar('HelioprojectiveXT', bound=kgpy.uncertainty.ArrayLike)
-HelioprojectiveYT = typ.TypeVar('HelioprojectiveYT', bound=kgpy.uncertainty.ArrayLike)
+RadianceInputT = typ.TypeVar('RadianceInputT', bound=kgpy.optics.vectors.TemporalSpectralFieldVector)
+RadianceOutputT = typ.TypeVar('RadianceOutputT', bound=kgpy.uncertainty.ArrayLike)
+
+SpectralRadianceInputT = typ.TypeVar('SpectralRadianceInputT', bound=kgpy.optics.vectors.TemporalOffsetSpectralFieldVector)
+SpectralRadianceOutputT = typ.TypeVar('SpectralRadianceOutputT', bound=kgpy.uncertainty.ArrayLike)
 
 
 @dataclasses.dataclass(eq=False)
-class HelioprojectiveCartesianVector(
-    kgpy.vectors.AbstractVector,
-    typ.Generic[HelioprojectiveXT, HelioprojectiveYT],
-):
-    helioprojective_x: HelioprojectiveXT = 0 * u.arcsec
-    helioprojective_y: HelioprojectiveYT = 0 * u.arcsec
-
-
-@dataclasses.dataclass(eq=False)
-class TemporalSpectralHelioprojectiveCartesianVector(
-    typ.Generic[TimeT, WavelengthT, HelioprojectiveXT, HelioprojectiveYT],
-    HelioprojectiveCartesianVector[HelioprojectiveXT, HelioprojectiveYT],
-    kgpy.vectors.SpectralVector[WavelengthT],
-    kgpy.vectors.TemporalVector[TimeT],
+class Radiance(
+    kgpy.function.Array[RadianceInputT, RadianceOutputT]
 ):
     pass
 
 
 @dataclasses.dataclass(eq=False)
-class RadiantIntensity(
-    kgpy.function.Array[
-        TemporalSpectralHelioprojectiveCartesianVector[
-            kgpy.uncertainty.ArrayLike,
-            kgpy.uncertainty.ArrayLike,
-            kgpy.labeled.WorldCoordinateSpace[kgpy.vectors.Cartesian2D],
-            kgpy.labeled.WorldCoordinateSpace[kgpy.vectors.Cartesian2D],
-        ],
-        kgpy.uncertainty.ArrayLike,
-    ]
+class SpectralRadiance(
+    kgpy.function.Array[SpectralRadianceInputT, SpectralRadianceOutputT]
 ):
     pass
