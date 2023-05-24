449cd5a058aeed6ed60f9d6270f7462023c6484e
Roy Smart <roytsmart@gmail.com>
2023-05-24 10:48:49 -0600

    Updates to `kgpy.observatories.aia`

--- kgpy/observatories/sdo/aia/__init__.py
+++ kgpy/observatories/sdo/aia/__init__.py
@@ -11,33 +11,106 @@ import sunpy.map
 import sunpy.net.attrs
 import astropy.units as u
 import aiapy.calibrate
+import kgpy.labeled
+import kgpy.vectors
+import kgpy.function
+import kgpy.optics
 import kgpy.obs
+import kgpy.solar
 
 
-@dataclasses.dataclass
-class AIA(kgpy.obs.Image):
-    """
-    A class for storing downloading and storing a sequence of AIA images
-    """
-
+@dataclasses.dataclass(eq=False)
+class RadiantIntensity(
+    kgpy.solar.Radiance[
+        kgpy.optics.vectors.TemporalSpectralFieldVector[
+            kgpy.labeled.Array,
+            kgpy.labeled.Array,
+            kgpy.labeled.WorldCoordinateSpace,
+            kgpy.labeled.WorldCoordinateSpace,
+        ],
+        kgpy.labeled.Array,
+    ],
+):
     @classmethod
-    def from_path_array(
-            cls,
-            path_array: np.ndarray,
-    ) -> 'AIA':
+    def from_path_array(cls, path_array: kgpy.labeled.Array[pathlib.Path]):
 
-        aia_map = sunpy.map.Map(path_array[0, 0])
-        self = cls.zeros(path_array.shape + aia_map.data.shape)
-        self.channel = np.zeros(path_array.shape[~0]) * u.AA
+        shape_base = path_array.shape
+        # map_prototype = sunpy.map.Map(path_array[dict(time=0, wavelength=0)].array)
+        # intensity_prototype = kgpy.labeled.Array(map_prototype.data, axes=['pixel_x', 'pixel_y'])
+        shape_wcs = dict(pixel_x=4096, pixel_y=4096)
+        shape = dict(**shape_base, **shape_wcs)
 
-        for i in range(path_array.shape[0]):
-            for c in range(path_array.shape[1]):
-                aia_map = sunpy.map.Map(path_array[i, c])
-                self.intensity[i, c] = aia_map.data * u.adu
-                self.time[i, c] = aia_map.date
-                self.exposure_length[i, c] = aia_map.exposure_time
-                self.channel[c] = aia_map.wavelength
-                self.wcs[i, c] = aia_map.wcs
+        time = kgpy.labeled.Array(astropy.time.Time(np.zeros(tuple(shape_base.values())), format='unix'), axes=path_array.axes)
+        self = cls(
+            input=kgpy.optics.vectors.TemporalSpectralFieldVector(
+                time=time,
+                wavelength=kgpy.labeled.Array.empty(dict(wavelength=shape['wavelength'])) * u.AA,
+                field_x=kgpy.labeled.WorldCoordinateSpace(
+                    crval=kgpy.labeled.Array.zeros(shape_base) * u.arcsec,
+                    crpix=kgpy.vectors.CartesianND.from_components(
+                        pixel_x=kgpy.labeled.Array.zeros(shape_base) * u.pix,
+                        pixel_y=kgpy.labeled.Array.zeros(shape_base) * u.pix,
+                    ),
+                    cdelt=kgpy.labeled.Array.zeros(shape_base) * (u.arcsec / u.pix),
+                    pc_row=kgpy.vectors.CartesianND.from_components(
+                        pixel_x=kgpy.labeled.Array.zeros(shape_base),
+                        pixel_y=kgpy.labeled.Array.zeros(shape_base),
+                    ),
+                    shape_wcs=shape_wcs,
+                ),
+                field_y=kgpy.labeled.WorldCoordinateSpace(
+                    crval=kgpy.labeled.Array.zeros(shape_base) * u.arcsec,
+                    crpix=kgpy.vectors.CartesianND.from_components(
+                        pixel_x=kgpy.labeled.Array.zeros(shape_base) * u.pix,
+                        pixel_y=kgpy.labeled.Array.zeros(shape_base) * u.pix,
+                    ),
+                    cdelt=kgpy.labeled.Array.zeros(shape_base) * (u.arcsec / u.pix),
+                    pc_row=kgpy.vectors.CartesianND.from_components(
+                        pixel_x=kgpy.labeled.Array.zeros(shape_base),
+                        pixel_y=kgpy.labeled.Array.zeros(shape_base),
+                    ),
+                    shape_wcs=shape_wcs,
+                ),
+            ),
+            output=kgpy.labeled.Array.zeros(shape) * u.DN,
+        )
+
+        ix = ~0
+        iy = ~1
+
+        axes_spatial = [None, None]
+        axes_spatial[ix] = 'pixel_x'
+        axes_spatial[iy] = 'pixel_y'
+
+        for index in path_array.ndindex():
+
+            aia_map = sunpy.map.Map(path_array[index].array)
+
+            intensity = kgpy.labeled.Array(aia_map.data << u.DN, axes=axes_spatial)
+            index_intensity = dict(
+                **index,
+                pixel_x=slice(intensity.shape['pixel_x']),
+                pixel_y=slice(intensity.shape['pixel_y']),
+            )
+
+            self.output[index_intensity] = intensity
+            # print(aia_map.date)
+            self.input.time[index] = aia_map.date
+            self.input.wavelength[dict(wavelength=index['wavelength'])] = aia_map.wavelength
+
+            self.input.field_x.crval[index] = aia_map.wcs.wcs.crval[ix]
+            self.input.field_x.crpix.pixel_x[index] = aia_map.wcs.wcs.crpix[ix] << u.pix
+            self.input.field_x.crpix.pixel_y[index] = aia_map.wcs.wcs.crpix[iy] << u.pix
+            self.input.field_x.cdelt[index] = aia_map.wcs.wcs.cdelt[ix] << (u.deg / u.pix)
+            self.input.field_x.pc_row.pixel_x[index] = aia_map.wcs.wcs.pc[ix, ix]
+            self.input.field_x.pc_row.pixel_y[index] = aia_map.wcs.wcs.pc[ix, iy]
+
+            self.input.field_y.crval[index] = aia_map.wcs.wcs.crval[iy]
+            self.input.field_y.crpix.pixel_x[index] = aia_map.wcs.wcs.crpix[ix] << u.pix
+            self.input.field_y.crpix.pixel_y[index] = aia_map.wcs.wcs.crpix[iy] << u.pix
+            self.input.field_y.cdelt[index] = aia_map.wcs.wcs.cdelt[iy] << (u.deg / u.pix)
+            self.input.field_y.pc_row.pixel_x[index] = aia_map.wcs.wcs.pc[iy, ix]
+            self.input.field_y.pc_row.pixel_y[index] = aia_map.wcs.wcs.pc[iy, iy]
 
         return self
 
@@ -87,6 +160,7 @@ class AIA(kgpy.obs.Image):
             for file in files:
                 file_15 = level_15_path / pathlib.Path(file).name
                 files_15.append(file_15)
+                # files_15.append(file)
                 if not file_15.is_file():
                     aia_map = sunpy.map.Map(file)
                     aia_map = aiapy.calibrate.update_pointing(aia_map)
@@ -95,6 +169,6 @@ class AIA(kgpy.obs.Image):
 
             file_paths.append(files_15)
 
-        file_paths = np.array(file_paths).T
+        file_paths = kgpy.labeled.Array(np.array(file_paths).T, axes=['time', 'wavelength'])
 
         return cls.from_path_array(file_paths)
--- kgpy/observatories/sdo/aia/__init__test.py
+++ kgpy/observatories/sdo/aia/__init__test.py
@@ -3,24 +3,20 @@ import pathlib
 import astropy.units as u
 import astropy.time
 from kgpy.obs import __init__test
-from . import AIA
+from . import RadiantIntensity
 
 
 @pytest.fixture(scope='module')
-def obs_test() -> AIA:
+def obs_test() -> RadiantIntensity:
     time_start = astropy.time.Time('2019-09-30T00:00:00')
     time_end = time_start + 30 * u.s
     download_path = pathlib.Path(__file__).parent / 'test_jsoc'
     channels = [171, 304] * u.AA
-    return AIA.from_time_range(time_start, time_end, download_path, channels=channels)
+    return RadiantIntensity.from_time_range(time_start, time_end, download_path, channels=channels)
 
 
-class TestAIA(__init__test.TestImage):
+class TestRadiantIntensity:
 
-    def test_from_time_range(self,  obs_test: AIA):
+    def test_from_time_range(self,  obs_test: RadiantIntensity):
 
-        assert obs_test.intensity.sum() > 0
-
-        sh = obs_test.intensity.shape[:2]
-        assert obs_test.time.shape == sh
-        assert obs_test.exposure_length.shape == sh
+        assert obs_test.output.sum() > 0
