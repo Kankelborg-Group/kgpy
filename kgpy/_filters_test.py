4352253c8fb46141fed275d542bea840d656ca27
Roy Smart <roytsmart@gmail.com>
2023-10-31 10:34:27 -0600

    Moved `kgpy._filters_test.test_mean_trimmed` to `test_mean_trimmed_numba`.

--- kgpy/_filters_test.py
+++ kgpy/_filters_test.py
@@ -20,15 +20,16 @@ def test_gaussian_trimmed():
     assert b.sum() != 0
 
 
-def test_mean_trimmed():
+def test_mean_trimmed_numba():
 
     shape = (100, 100)
     a = np.random.normal(loc=100, scale=25, size=shape)
 
-    b = kgpy.filters.mean_trimmed(
+    b = kgpy.filters.mean_trimmed_numba(
         array=a,
-        kernel_size=21,
-        proportion=0 * u.percent
+        kernel_shape=21,
+        proportion=0.25,
+        axis=1,
     )
 
     assert b.shape == shape
